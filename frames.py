8696a1cda3a151903aac83b16f898cfb33751aaf
Sudarshan M <sudarshanr>
2022-05-05 12:57:48 -0700

    First Copy

--- frames.py
+++ frames.py
@@ -2,12 +2,14 @@ from picamera import PiCamera
 import time
 import os
 import subprocess
+import cv2
 
 
 class Frames():
     def __init__(self):
         self.time_to_execute = 10
         self.dict_for_time = dict()
+        
 
     def record_video(self):
         camera = PiCamera()
@@ -18,24 +20,29 @@ class Frames():
         print("invoked...")
         # os.system(cmd)
         start_time = time.time()
-        camera.resolution = (1280, 720)
+        # camera.resolution = (1280, 720)
+        camera.resolution = (160, 160)
         camera.vflip = True
         camera.contrast = 10
         executed_time = 0
         video_number = 1
         print("starting to record")
         while executed_time < self.time_to_execute:
-            video_name = "video" + str(video_number) + ".h264"
+            # video_name = "video" + str(video_number) + ".h264"
+            video_name = str(video_number) + ".h264"
             file_name = "/home/pi/videos/" + video_name
             self.dict_for_time[video_name] = time.time()
-            print("Start recording...")
+            # print("Start recording...")
             camera.start_recording(file_name)
             camera.wait_recording(0.5)
             camera.stop_recording()
-            print("Done.")
+            # print("Done.")
             executed_time += 0.5
             video_number += 1
         print("Total time taken: ", time.time() - start_time)
+    
+
+    
 
 if __name__ == "__main__":
     my_frames = Frames()
--- upload_to_s3.py
+++ upload_to_s3.py
@@ -3,23 +3,31 @@ import boto3
 import io
 import time
 import argparse
+import cv2
 
 class UploadVideo():
     def __init__(self):
         self.aws_region = "us-east-one"
         self.s3_input_bucket_name = "cse546group27inputvideobucket"
         self.video_path = "/home/pi/videos/"
-    
+        self.sqs_service = boto3.resource("sqs")
+        self.timeline_dict = dict()
+        self.sqs_response_queue_name = "response-queue"
+
     def video_upload(self, total_number = 600):
         boto3_session = boto3.Session()
         s3 = boto3_session.client("s3")
         number = 1
         time.sleep(3)
         while number <= total_number:
-            file_name = "video" + str(number) + ".h264"
-            path_of_video_file = self.video_path + file_name
-            self.upload_video_to_s3(s3, path_of_video_file, file_name)
-            print("upload success")
+            # file_name = "video" + str(number) + ".h264"
+            file_name = str(number) + ".h264"
+            # path_of_video_file = self.video_path + file_name
+            path_of_video_file = self.extract_frames(file_name)
+            # print("Video Name :", path_of_video_file)
+            self.upload_video_to_s3(s3, path_of_video_file, path_of_video_file.split("/")[-1])
+            self.timeline_dict[str(number)] = time.time()
+            # print("upload success")
             number += 1
             time.sleep(0.5)
         return
@@ -29,13 +37,53 @@ class UploadVideo():
         # with open(file) as f:
         files = io.open(file, "rb", buffering = 0)
         s3_client.upload_fileobj(io.BytesIO(files.read()), self.s3_input_bucket_name, file_name)
-        print("Video uploaded to S3")
+        # print("Video uploaded to S3")
         return
+    
+    def fetch_result_from_sqs(self):
+        queue = self.sqs_service.get_queue_by_name(QueueName=self.sqs_response_queue_name)
+        messages = queue.receive_messages(MaxNumberOfMessages=1, WaitTimeSeconds=20)
+        if messages:
+            for m in messages:
+                content = json.loads(m.body)
+                m.delete()
+                pic = list(content.keys())[0]
+                
+                picture_name = pic.split('.')[0]
+                # print("Pic: ", pic)
+                # print("Picture Name: ", picture_name)
+                # print("content: ", list(content.keys())[0])
+                print(f"The person {picture_name} recognized: ", content[pic])
+                print("Latency: ",time.time() - self.timeline_dict[picture_name])
+                # print("content", list(content.values())[0])
+        return
+
+    def extract_frames(self, file_name):
+        # Read the video from specified path
+        dimensions = (160, 160)
+        cam = cv2.VideoCapture(self.video_path + file_name)
+        ret, frame = cam.read()
+        if ret:
+            image_name = self.video_path + file_name.split(".")[0] + ".jpg"
+            cv2.imwrite(image_name, frame)
+            # img = cv2.imread(image_name, cv2.IMREAD_UNCHANGED)
+            # resized = cv2.resize(img, (160, 160), interpolation = cv2.INTER_AREA)
+            # cv2.imwrite(image_name, resized)
+            # print("Width: ", resized.shape[1])
+            # print("Length: ", resized.shape[0])
+        cam.release()
+        cv2.destroyAllWindows()
+        return image_name
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--number", help = "Denotes number of videos will be taken")
     args= parser.parse_args()
-    print("Argument number received: ", args.number)
+    # print("Argument number received: ", args.number)
+    number_of_invocations = int(args.number)
     my_upload  = UploadVideo()
-    my_upload.video_upload(total_number=int(args.number))
+    my_upload.video_upload(total_number=number_of_invocations)
+    my_upload.fetch_result_from_sqs()
+    i = 0
+    while i < number_of_invocations:
+        my_upload.fetch_result_from_sqs()
